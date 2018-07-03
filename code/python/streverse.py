e7e3574aad8a8a1364280a0f93d73fc0ac409658
Aman Jaiswal <amanjai01@gmail.com>
2018-07-03 16:26:26 +0530

    Delete streverse.py

--- code/python/streverse.py
+++
@@ -1,7 +0,0 @@
-text=input('Enter a string:')
-reverse=''
-#to perform reverse of the string
-#in range, first 2 arg define the range of values to be used, and the last determines the op to be performed
-for i in range(len(text)-1,-1,-1):
-    reverse+=text[i]
-print(reverse)
