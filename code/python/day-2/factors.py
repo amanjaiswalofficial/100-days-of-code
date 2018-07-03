eab42fda1d48af605f4e87bf40ac56064af90cff
Aman Jaiswal <amanjai01@gmail.com>
2018-07-03 16:29:36 +0530

    Update factors.py

--- code/python/day-2/factors.py
+++ code/python/day-2/factors.py
@@ -1,6 +1,8 @@
 num=int(input('Enter a number to find it\'s factors:'))
+#initializing a factors named list to add numbers
 factors=[]
+#loop to check for each number from 1 to the number itself whether it's a factor of it
 for i in range(1,num+1):
-    if(num%i==0):
-        factors.append(i)
+    if(num%i==0):#condition to check the above
+        factors.append(i)#on true add the number to the list
 print(factors)
